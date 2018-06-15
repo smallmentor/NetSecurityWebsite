f6111dabad7be2da82ccaa3e5634bf28b72f26af
smallmentor <s92203aaa@gmail.com>
2018-06-15 15:56:50 +0800

    所有作業均已完成 下一步將開始更改自訂版面

---
+++ js/head.js
@@ -0,0 +1,10 @@
+document.writeln("<meta charset=\'utf-8\' />");
+document.writeln("    <title>網路安全作業</title>");
+document.writeln("    <!--CSS-->");
+document.writeln("    <link rel=\'stylesheet\' href=\'css/bootstrap.min.css\' />");
+document.writeln("	<link rel=\'stylesheet\' href=\'css/navbar_left.css\' />");
+document.writeln("	<link rel=\'stylesheet\' href=\'css/font.css\' />");
+document.writeln("    <!--外接JavaScript-->");
+document.writeln("    <script src=\'https://code.jquery.com/jquery-3.2.1.slim.min.js\' integrity=\'sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\' crossorigin=\'anonymous\'></script>");
+document.writeln("    <script src=\'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\' integrity=\'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\' crossorigin=\'anonymous\'></script>");
+document.writeln("    <script src=\'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\' integrity=\'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\' crossorigin=\'anonymous\'></script>");
